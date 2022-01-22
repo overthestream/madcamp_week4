de04145538f8248f8ad640274fa9f2000a3e151b
sunwookim <sunkim09@g.skku.edu>
2022-01-22 21:14:42 +0900

    add comment

--- Tab1/GHT.js
+++ Tab1/GHT.js
@@ -6,7 +6,9 @@ import {
     View, 
     ScrollView,
     SafeAreaView,
-    TouchableOpacity
+    TouchableOpacity,
+    TextInput,
+    Keyboard
 } from 'react-native';
 import Talk from './Talk';
 import { SwipeListView } from 'react-native-swipe-list-view'
@@ -17,7 +19,10 @@ function GHT() {
         .fill('')
         .map(() => ({Talk}));
 
+    const LIST_COMMENT = ["밤산은 인정이지", "낭만 미쳤다", "TPS 가야지", "아 진짜 너무 졸리다", "하나 둘 셋 화이팅~!"]
     
+    const [memo, setMemo] = useState("다같이 인생네컷 찍었다아아아아아아아아아아아아아");
+    const [newComment, setNewComment] = useState('');
 
     return (
         <SafeAreaView style={styles.container}>
@@ -31,19 +36,43 @@ function GHT() {
                 renderHiddenItem={(data, rowMap) => (
                 <View style={styles.swipeHiddenItemContainer}>
                     <TouchableOpacity
-                    onPress={() => {}}>
+                        onPress={() => {}}>
                         <View 
                             style={styles.swipeHiddenLeftItem}>
-                            <Text style={styles.swipeHiddenLeftItemText}>다같이 인생네컷 찍었다아아아아아아아아아아아아아</Text>
+                            <Text style={styles.swipeHiddenLeftItemText}>{memo}</Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity
-                    onPress={() => {}}>
+                    {/* <TouchableOpacity
+                        onPress={() => {}}>  */}
                         <View
                             style={styles.swipeHiddenRightItem}>
-                            <Text style={styles.swipeHiddenRightItemText}>right</Text>
+                            <View
+                                style={styles.swipeHiddenCommentItem}>
+                                <ScrollView>
+                                    {LIST_COMMENT.map(comment => {
+                                        var a = <Text
+                                            style={styles.swipeHiddenCommentItemText}>{comment}</Text>
+                                        return a
+                                    })}
+                                </ScrollView>
+                            </View>
+                            <View
+                                style={styles.swipeHiddenInputItem}>
+                                <TextInput
+                                    style={styles.swipeHiddenInputTextItem}
+                                    value={newComment}
+                                    onChangeText={setNewComment}
+                                    placeholder='댓글 입력' />
+                                <TouchableOpacity
+                                    activeOpacity={0.8}
+                                    style={styles.swipeHiddenButtonItem}
+                                    onPress={() => {Keyboard.dismiss()}}>
+                                    <Text
+                                        style={{color: '#3D56B2'}}>게시</Text>
+                                </TouchableOpacity>
+                            </View>
                         </View>
-                    </TouchableOpacity>
+                    {/* </TouchableOpacity> */}
                 </View>
                 )}
                 leftOpenValue={140}
@@ -96,8 +125,9 @@ const styles = StyleSheet.create({
     swipeHiddenRightItem: {
         width: 210,
         height: 200,
-        justifyContent: 'center',
+        //justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'column',
         marginLeft: 10,
         marginRight: 10,
         marginTop: 15,
@@ -107,9 +137,51 @@ const styles = StyleSheet.create({
         fontSize: 14,
     },
     swipeHiddenRightItemText: {
-        fontSize: 20,
+        fontSize: 10,
     },
-
+    swipeHiddenCommentItem: {
+        width: 200,
+        height: 120,
+        flexDirection: 'row',
+        // justifyContent: 'center',
+        //alignItems: 'center',
+        marginLeft: 5,
+        marginRight: 5,
+        marginBottom: 5,
+        //marginTop: 5,
+        padding: 5,
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 20,
+    },
+    swipeHiddenCommentItemText: {
+        fontSize: 14,
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 5,
+    },
+    swipeHiddenInputItem: {
+        width: 200,
+        height: 40,
+        flexDirection: 'row',
+        borderColor: 'black',
+        borderWidth: 1,
+        borderRadius: 20,
+        marginTop: 5,
+    },
+    swipeHiddenInputTextItem: {
+        height: 40,
+        width: 150,
+        //borderWidth: 1,
+        //margin: 5,
+        padding: 10,
+        borderRadius: 20,
+    },
+    swipeHiddenButtonItem: {
+        height: 40,
+        width: 60,
+        padding: 10,
+    }
 });
 
 export default GHT;
