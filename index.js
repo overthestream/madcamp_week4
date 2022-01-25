40bcfe65bf17b94adfede0a04acb331b703995a0
Hyojae Kwon <khj010909@gm.gist.ac.kr>
2022-01-25 21:52:10 +0900

    Connect

--- index.js
+++ index.js
@@ -3,9 +3,8 @@
  */
 
 import { AppRegistry } from 'react-native';
-import Meet from './tab1/meet';
-
-import App from './visit/VisitTab';
+// import Meet from './tab1/meet';
+import App from './App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => Meet);
+AppRegistry.registerComponent(appName, () => App);
