9f04fb30b7827269ce95adadc8a3d4c56c6ae8cd
Hyojae Kwon <khj010909@gm.gist.ac.kr>
2022-02-08 21:58:06 +0900

    error resolved

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import GHT from './tab1/GHT';
 // import Talk from './Tab1/Talk';
-import Meet from './tab1/meet';
+import Meet from './tab2/meet';
 // import EventDay from './Tab1/EventDay';
 import VisitTab from './visit/VisitTab';
 import { createStackNavigator } from '@react-navigation/stack';
 import { getFocusedRouteNameFromRoute } from '@react-navigation/native';
-import MeetMainPage from './tab1/meet';
-import WhenToMeet from './tab1/make/makemeet';
+import MeetMainPage from './tab2/meet';
+import WhenToMeet from './tab2/make/makemeet';
 import React, { useLayoutEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import SearchLocation from './tab1/make/setlocation';
+import SearchLocation from './tab2/make/setlocation';
 import Login from './Login/Login';
-import MeetingDetail from './tab1/meetdetail/meetdetail';
+import MeetingDetail from './tab2/meetdetail/meetdetail';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
